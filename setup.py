3c5e091d9d58e7fd27d93c5bb278b57af956cb14
Mohammad Ghassemi <deskool@gmail.com>
2019-02-27 21:08:27 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simple_nlp_mmg',
+      version='0.1',
+      description='A set of tools to simplify NLP',
+      url='http://github.com/deskool/simpleNLP',
+      author='Mohammad M. Ghassemi',
+      author_email='ghassemi@alum.mit.edu',
+      license='MIT',
+      packages=['simple_nlp_mmg'],
+      zip_safe=False)
